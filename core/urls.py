4529830026783c9d893d2ed1826d8fe4c8bbe9bd
Shafwan Aminudin M <shafwanaminudinm@gmail.com>
2024-08-05 11:20:33 +0700

    Add application settings page (available: manage subject).

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,8 @@ from core import main
 
 urlpatterns = [
     path('', main.home, name='home'),
+    path('settings/', main.app_settings, name='app_settings'),
+    path('settings/subjects/', main.subject_page, name='subject_page'),
+    path('settings/subjects/<uuid:subject_id>/', main.subject, name='subject'),
+    path('settings/subjects/create/', main.subject, name='create_subject'),
 ]
