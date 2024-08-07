d0aa4efba261b36e01cef981dc995b56bc668940
Izatou <faris.4haq@gmail.com>
2024-08-08 05:37:44 +0700

    Merge for login

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from core import main
+from .views import index,search
 
 urlpatterns = [
     path('', main.home, name='home'),
+    path('todos/', index, name='index'),
+    path('search/', search, name='search'),  # new
 ]
