5b67f8c5f585ceb478371ad4f7c4f7b17e232df1
Izatou <faris.4haq@gmail.com>
2024-08-08 07:56:55 +0700

    Login page

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,10 @@ from .views import index,search
 urlpatterns = [
     path('', main.home, name='home'),
     path('todos/', index, name='index'),
+    path('signin/', main.signin, name='signin'),
+    path('signin/user/', main.signin, name='user_signin'),
     path('search/', search, name='search'),  # new
+
     path('settings/', main.app_settings, name='app_settings'),
     path('settings/subjects/', main.subject_page, name='subject_page'),
     path('settings/subjects/<uuid:subject_id>/', main.subject, name='subject'),
