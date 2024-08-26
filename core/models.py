7bb9f2543a4770acd4d76ea042f5fc6e00f2f7d2
Shafwan Aminudin M <shafwanaminudinm@gmail.com>
2024-08-26 08:10:19 +0700

    Make the reflection question and journal available for any subject.

--- core/models.py
+++ core/models.py
@@ -52,22 +52,11 @@ class Subject(models.Model):
     def __str__(self):
         return f'{self.id} {self.name}'
     
-class SubjectReflection(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    subject = models.ForeignKey(Subject, blank=True, null=True, on_delete=models.CASCADE)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(default=timezone.now)
-    
-    def __str__(self):
-        return f'{self.id} {self.subject}'
-    
 class ReflectionQuestion(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    subject_reflection = models.ForeignKey(SubjectReflection, blank=True, null=True, on_delete=models.CASCADE)
     question = models.TextField(blank=True, null=True)
     articles = models.ManyToManyField(Article, related_name="reflection_questions")
     order = models.IntegerField(null=True, blank=True)
-    show = models.BooleanField(null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(default=timezone.now)
     
@@ -88,9 +77,12 @@ class TeacherAgenda(models.Model):
 
 class Journal(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    subject = models.ForeignKey(Subject, blank=True, null=True, on_delete=models.SET_NULL)
+    subject_text = models.CharField(max_length=255, blank=True, null=True)
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
     agenda = models.ForeignKey(TeacherAgenda, blank=True, null=True, on_delete=models.CASCADE)
     question = models.ForeignKey(ReflectionQuestion, blank=True, null=True, on_delete=models.SET_NULL)
+    question_text = models.TextField(blank=True, null=True)
     content = models.TextField(blank=True, null=True) # jawaban dari pertanyaan refleksi
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(default=timezone.now)
