c0d2b6ce654c76fd6c64c0206bdcdc564d69415c
Shafwan Aminudin M <shafwanaminudinm@gmail.com>
2024-08-27 23:50:58 +0700

    Manage reflection question page.

--- core/admin.py
+++ core/admin.py
@@ -15,15 +15,10 @@ class SubjectAdmin(admin.ModelAdmin):
     date_hierarchy='created_at'
 admin.site.register(Subject, SubjectAdmin)
 
-class SubjectReflectionAdmin(admin.ModelAdmin):
-    list_display = ['id', 'subject', 'updated_at', 'created_at',]
-    search_fields = ['id', 'subject__id', 'subject__name',]
-    date_hierarchy='created_at'
-    
 class ReflectionQuestionAdmin(admin.ModelAdmin):
-    list_display = ['id', 'subject_reflection', 'order', 'show', 'updated_at', 'created_at',]
-    search_fields = ['id', 'subject_reflection__id', 'question']
-    date_hierarchy='created_at'
+    list_display = ['id', 'question', 'order', 'updated_at', 'created_at',]
+    search_fields = ['id', 'question']
+    order='order'
 admin.site.register(ReflectionQuestion, ReflectionQuestionAdmin)
 
 class TeacherAgendaAdmin(admin.ModelAdmin):
